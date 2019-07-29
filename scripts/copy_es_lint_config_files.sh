77e8b7b07d5a466a5928b2c6ef030aa3b140e4d3
Mariano Martín <mariano.martin@sage.com>
2019-07-29 14:58:05 +0200

    Adding script to copy configuration files for eslint

---
+++ scripts/copy_es_lint_config_files.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/Sage/carbon-factory/master/.eslintrc
+sudo mkdir -p /home/developer/sites/gac/s1_gac_ui/node_modules/carbon-factory
+sudo cp .eslintrc /home/developer/sites/gac/s1_gac_ui/node_modules/carbon-factory/.eslintrc
+rm .eslintrc
+
