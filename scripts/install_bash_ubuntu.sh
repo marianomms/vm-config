7c4c3f5b6ce5f5456ad3528fbcb9413a8b8bce76
Mariano Martín Sánchez <mariano.martin@sage.com>
2019-10-31 17:22:28 +0100

    Update install_bash_ubuntu.sh

--- scripts/install_bash_ubuntu.sh
+++ scripts/install_bash_ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install openssh-server tmux ifupdown ack tilix
+sudo apt install openssh-server tmux ifupdown ack tilix git-cola
 
 cp -r ../bash/. ~/
 echo 'source ~/.bashrc_mms' >> ~/.bashrc
