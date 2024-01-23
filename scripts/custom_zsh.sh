14db0ef3ad34cc9bdc2c707e8965668bbac3b907
Mariano Martín Sánchez <mariano.martin@sage.com>
2024-01-23 15:53:52 +0000

    wip

--- scripts/custom_zsh.sh
+++ scripts/custom_zsh.sh
@@ -1,7 +1,7 @@
-echo "*** CUSTOM ZSH: INSTALLING Oh My Zsh ***"
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-zsh
-echo
+# echo "*** CUSTOM ZSH: INSTALLING Oh My Zsh ***"
+# wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+# zsh
+# echo
 
 echo "*** CUSTOM ZSH: INSTALLING  zsh-syntax-highlighting ***"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
